1213834af923bee82b4872a9ddc6c9d6268d99a4
Uwe Eisele <uwe.eisele@online.de>
2019-01-05 22:01:54 +0100

    Learn Functional Programming: Adjust exercise1 answer of chapter recursion

--- code-examples/learn_functional_programming_with_elixir/recursion/answers/exercise_1.ex
+++ code-examples/learn_functional_programming_with_elixir/recursion/answers/exercise_1.ex
@@ -7,21 +7,15 @@
 # Visit http://www.pragmaticprogrammer.com/titles/cdc-elixir for more book information.
 #---
 defmodule MyList do
+  
   def max([]), do: nil
   def max([a]), do: a
-  def max([a, b | rest]) when a >= b, do: find_max(rest, a)
-  def max([a, b | rest]) when a < b, do: find_max(rest, b)
-
-  defp find_max([], max), do: max
-  defp find_max([head | rest], max) when head >= max, do: find_max(rest, head)
-  defp find_max([head | rest], max) when head < max, do: find_max(rest, max)
+  def max([a, b | rest]) when a >= b, do: max([a | rest])
+  def max([a, b | rest]) when a < b, do: max([b | rest])
 
   def min([]), do: nil
   def min([a]), do: a
-  def min([a, b | rest]) when a <= b, do: find_min(rest, a)
-  def min([a, b | rest]) when a > b, do: find_min(rest, b)
+  def min([a, b | rest]) when a <= b, do: min([a | rest])
+  def min([a, b | rest]) when a > b, do: min([b | rest])
 
-  defp find_min([], min), do: min
-  defp find_min([head | rest], min) when head <= min, do: find_min(rest, head)
-  defp find_min([head | rest], min) when head > min, do: find_min(rest, min)
 end
